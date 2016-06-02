d2b56fa435654612901e90ed7f50575dde6f7f7a
GregoirePiat <gregoire.piat@outlook.fr>
2016-06-02 08:05:00 +0200

    Création de la vue

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,11 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
 
+public class Controller {
+    public Label coucou;
+    public void sayCoucou(ActionEvent actionEvent) {
+        coucou.setText(" nouvelle montre");
+    }
 }
---
+++ src/sample/Difficulty.java
@@ -0,0 +1,13 @@
+package sample;
+
+/**
+ * Created by GregoirePiat on 19/05/16.
+ */
+public interface Difficulty {
+
+    public static final int DIFFICULTY = 1;
+
+    public static final int NB_COLUMNS = 20;
+
+    public static final int NB_BOMBS = 10;
+}
