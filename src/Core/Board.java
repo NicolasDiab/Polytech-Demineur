4ac102eebedb546540daab397289da55f115fa26
NicolasDiab <nicolas-diab@hotmail.fr>
2016-06-02 11:05:52 +0200

    first code - need to test

---
+++ src/Core/Board.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Core;
+
+import java.util.*;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class Board {
+    private final Square[][] board;
+    private final int rows;
+    private final int columns;
+    private final int mines;
+    
+    public Board(int rows, int columns, int nbMine) {
+        this.rows = rows;
+        this.columns = columns;
+        this.mines = nbMine;
+        
+        board = new Square[rows][columns];
+        
+        // add squares
+        addSquares();
+        
+        // add random mines
+        addMines();
+    }
+    
+    // Constructor method. Add squares to the map
+    private void addSquares() {
+        for(int i=0; i<rows; ++i) {
+            for(int j=0; j<columns; ++i) {
+                Square square = new Square(i, j);
+                board[i][j] = square;
+            }
+        }
+    }
+    
+    // Constructor method. Add mines to the squares
+    private void addMines() {
+        // add "mines" random mines to the board
+        Random rn = new Random();
+        for(int i=0; i<mines; ++i) {
+            int row;
+            int column;
+            do {
+            row = rn.nextInt(10); // int between 0 and 9
+            column = rn.nextInt(10);
+            } while(board[row][column].isMine());
+            Square square = board[row][column];
+            square.setMine(true);
+            
+            // for each neighbour incrementes the number of neighbour mines
+            List<Square> neighbours = getNeighbourSquares(row, column);
+            for(Square neighbour : neighbours) {
+                neighbour.incrementeNbNeighbourMines();
+            }
+        }
+    }
+    
+    // return a square's neighbours in a list of squares
+    private List<Square> getNeighbourSquares(int row, int column) {
+        ArrayList<Square> list = new ArrayList<>();
+        
+        // loop for 3*3 squares around the middle one
+        // if side board is reached don't loop in
+        int i = row == 0 ? row : row+1;
+        int maxI = row == this.rows ? row : row+1;
+        int j = column == 0 ? column : row+1;
+        int maxJ = column == this.columns ? column : column+1;
+        for(; i<maxI; ++i) {
+            for(; j<maxJ; ++i) {
+                if (i!=row && j!=column) {
+                    list.add(board[row-1][column-1]);
+                }
+            }
+        }
+        
+        return list;
+    }
+    
+    public int clic(int row, int column) {
+        Square square = board[row][column];
+        // if players clics on a mine, return -1, game over
+        if (square.isMine()) {
+            return -1;
+        }
+        // if players clics on an already visible square, nothing happens
+        if (square.isVisible()) {
+            return 0;
+        }
+        square.setVisible(true);
+        
+        discoverNeighbours(row, column);
+        if (gameIsWon()) return 1;
+        
+        return 0;
+    }
+    
+    // recursive method - turns neighbour squares visible
+    // stop recursity if neighbour is a mine or if square is visible (marked)
+    public void discoverNeighbours(int row, int column) {
+        List<Square> neighbours = getNeighbourSquares(row, column);
+        for(Square neighbour : neighbours) {
+            if(!neighbour.isMine()) {
+                // has a mine nerby
+                if (neighbour.getNbNeighbourMines() > 0) {
+                    neighbour.setVisible(true);
+                }
+                else { // recursivity
+                    neighbour.setVisible(true);
+                    discoverNeighbours(neighbour.getRow(), neighbour.getColumn());
+                }
+            }
+        }
+    }
+    
+    public boolean gameIsWon() {
+        for(int i=0; i<rows; ++i) {
+            for(int j=0; j<columns; ++i) {
+                Square square = board[i][j];
+                if(!square.isVisible() && !square.isMine()) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+}
---
+++ src/Core/Square.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Core;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class Square {
+    private boolean mine;
+    
+    private boolean visible;
+    private boolean flag;
+    private int nbNeighbourMines;
+    private int row;
+    private int column;
+    
+    public Square(int row, int column) {
+        this(false, row, column);
+    }
+    
+    public Square(boolean mine, int row, int column) {
+        this.row = row;
+        this.column = column;
+        this.mine = mine;
+        this.flag = false;
+        this.visible = false;
+        this.nbNeighbourMines = 0;
+    }
+
+    /**
+     * @return the mine
+     */
+    public boolean isMine() {
+        return mine;
+    }
+
+    /**
+     * @param mine the mine to set
+     */
+    public void setMine(boolean mine) {
+        this.mine = mine;
+    }
+
+    /**
+     * @return the visible
+     */
+    public boolean isVisible() {
+        return visible;
+    }
+
+    /**
+     * @param visible the visible to set
+     */
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
+    /**
+     * @return the flag
+     */
+    public boolean isFlag() {
+        return flag;
+    }
+
+    /**
+     * @param flag the flag to set
+     */
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    /**
+     * @return the nbNeighbour
+     */
+    public int getNbNeighbourMines() {
+        return nbNeighbourMines;
+    }
+
+    /**
+     * @param nbNeighbourMines the nbNeighbourMines to set
+     */
+    public void setNbNeighbourMines(int nbNeighbourMines) {
+        this.nbNeighbourMines = nbNeighbourMines;
+    }
+
+    public void incrementeNbNeighbourMines() {
+        this.nbNeighbourMines += 1;
+    }
+
+    /**
+     * @return the row
+     */
+    public int getRow() {
+        return row;
+    }
+
+    /**
+     * @return the column
+     */
+    public int getColumn() {
+        return column;
+    }
+    
+    
+}
