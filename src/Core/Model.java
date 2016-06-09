f5397bbc2b57765c66738a998909c2622c64d465
NicolasDiab <nicolas-diab@hotmail.fr>
2016-06-09 10:48:23 +0200

    merge

--- src/Core/Model.java
+++ src/Core/Model.java
@@ -12,19 +12,18 @@ import java.util.Observable;
  * @author Nicolas
  */
 public class Model extends Observable {
-    Board board;
     String state;
     int score;
     
-    public void clic(int row, int col) {
+    public void clic(Board board, int row, int col) {
         boolean status = board.gameIsWon();
         if (status) {
             state = "Victory";
-            
         } else if(board.squareIsMine(row, col)) {
             state = "Defeat";
         } else {
             state = "Running";
+            board.clic(row, col);
         }
         
         // notification de la vue, suite à la mise à jour des champs state et score
